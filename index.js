e04edac8750883bf3a257b3cd53322ac0ddfb766
Voichita Maria <sabo_voichita@yahoo.com>
2024-09-24 14:03:12 +0200

    add inputs area

--- index.js
+++ index.js
@@ -6,33 +6,41 @@ function createStructure() {
 }
 function createTable() {
   return `
-  <table id="booksTable">
-    <thead>
+ <table id="booksTable">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Book name</th>
+          <th>Book author</th>
+          <th>Pages</th>
+          <th>Status</th>
+          <th>Edit Book</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>The 5 am club</td>
+          <td>Robin Sharma</td>
+          <td>252</td>
+          <td>
+            <span class="tick">&#10004;</span>
+          </td>
+          <td>
+            <span class="edit">🖌</span>
+            <span class="delete">❌</span>
+          </td>
+        </tr>
+      </tbody>
       <tr>
-        <th>No</th>
-        <th>Book name</th>
-        <th>Book author</th>
-        <th>Pages</th>
-        <th>Status</th>
-        <th>Edit Book</th>
+        <td><input type="number" name="" id="numberInput" /></td>
+        <td><input type="text" name="" id="nameInput" /></td>
+        <td><input type="text" name="" id="authorInput" /></td>
+        <td><input type="number" name="" id="pagesInput" /></td>
+        <td></td>
+        <td></td>
       </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>The 5 am club</td>
-        <td>Robin Sharma</td>
-        <td>252</td>
-        <td>
-          <span class="tick">&#10004;</span>
-        </td>
-        <td>
-          <span class="edit">🖌</span>
-          <span class="delete">❌</span>
-        </td>
-      </tr>
-    </tbody>
-  </table>`;
+    </table>`;
 }
 
 function initEvents() {
